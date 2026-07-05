07cae29a87430193ff1a7902828646b6888ea64e
Debasis Ghosh <65386308+debdevops@users.noreply.github.com>
2026-07-05 22:58:25 +0530

    Update dependency injection

--- services/api/src/ServiceHub.Infrastructure/DependencyInjection.cs
+++ services/api/src/ServiceHub.Infrastructure/DependencyInjection.cs
@@ -69,7 +69,7 @@ public static class DependencyInjection
         services.TryAddScoped<IServiceBusClientFactory, ServiceBusClientFactory>();
         services.TryAddScoped<IMessageSender, MessageSender>();
         services.TryAddScoped<IMessageReceiver, MessageReceiver>();
-        services.TryAddScoped<IMessageOperationsService, MessageOperationsService>();
+        services.AddScoped<IMessageOperationsService, MessageOperationsService>();
 
         // Router depends on all registered ICloudMessagingProvider implementations.
         services.TryAddSingleton(sp => new ServiceHub.Infrastructure.Routing.CloudProviderRouter(sp.GetServices<ICloudMessagingProvider>()));
